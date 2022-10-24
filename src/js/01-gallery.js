295f6656162def713fafcfd67132d6bbe8ec1cbd
egarra <garikhabuzov@gmail.com>
2022-10-24 13:30:39 +0300

    home work 8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+// Дополнительный импорт стилей
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const onCreateGallery = () => {
+
+    const galleryWrap = document.querySelector('.gallery');
+
+    galleryItems.forEach(image => {
+        galleryWrap.insertAdjacentHTML(
+            'afterbegin', 
+            `<a class="gallery__link" href="${image.original}"><img class="gallery__image" src="${image.preview}" data-source="${image.original}" alt="${image.description}"></a>`);
+    })
+
+    const lightbox = new SimpleLightbox('.gallery a', {
+        captionsData: "alt",
+        captionDelay: 250,
+      })
+}
 
 console.log(galleryItems);
+onCreateGallery();
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,41 @@
+const feedBackForm = document.querySelector('.feedback-form');
+const { elements: {
+    email,
+    message
+}} = feedBackForm;
+
+const _ = require('lodash');
+
+const savedUserData = JSON.parse(localStorage.getItem('feedback-form-state')) || {
+    email: '',
+    message: ''
+}
+
+email.value = savedUserData.email;
+message.value = savedUserData.message;
+
+feedBackForm.addEventListener('input', _.throttle( () => {
+            
+    localStorage.setItem('feedback-form-state', 
+            JSON.stringify(
+                { 
+                email: email.value,
+                message: message.value
+                }
+            )
+        )
+    }, 500)
+)
+
+feedBackForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    console.log(localStorage.getItem('feedback-form-state'))
+    e.currentTarget.reset();
+    localStorage.removeItem('feedback-form-state');
+})
+
+
+
+
 
